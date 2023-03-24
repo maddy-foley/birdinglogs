8392a424e51269ac55d8a846f5e06c72cb7634d6
Maddy Foley <maddyfoley5@gmail.com>
2023-03-23 22:45:51 -0400

    added homepage with single bird for now

--- ghi/app/src/pages/HomePage.js
+++ ghi/app/src/pages/HomePage.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react"
+import { BirdCard } from "../components/birds/BirdCard";
+
+
 export function HomePage(){
+    const [catbird, setCatbird] = useState({});
+
+    const getBird = async() => {
+        const response = await fetch("http://localhost:8000/api/birds/766")
+        if (response.ok){
+            const data = await response.json();
+            console.log(data)
+            setCatbird(data)
+        }
+    }
+    useEffect(() => {
+        getBird();
+    }, [])
+
     return (
         <div className="bg-transparent">
+            <div className="flex justify-center">
+                <BirdCard bird={catbird}/>
+            </div>
+
         </div>
     )
 }
