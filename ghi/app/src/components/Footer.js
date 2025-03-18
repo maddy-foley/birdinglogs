ee3d82f99795e300a300e7be3710582672a6b094
Maddy Foley <maddyfoley5@gmail.com>
2025-03-18 13:15:20 -0400

    added correct github name

--- ghi/app/src/components/Footer.js
+++ ghi/app/src/components/Footer.js
@@ -3,7 +3,7 @@ export function Footer () {
         <div className="footer">
             <div className="flex flex-col items-center pt-5">
                 <div>
-                    <a href="https://github.com/Mad-foley"><i className="fa-3x fa-brands fa-github footer-icon"></i></a>
+                    <a href="https://github.com/maddy-foley"><i className="fa-3x fa-brands fa-github footer-icon"></i></a>
                 </div>
                 <div>Developed By Maddy Foley</div>
             </div>
